cdb6b8d48aa210e4a85879366e16afa2375aacca
antonyCastineiras <antonycastineiras80@gmail.com>
2016-11-10 13:03:37 +0000

    displays a single ote on hash change

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 noteController = new NoteController(new NoteList());
+
+
+
--- note-controller.js
+++ note-controller.js
@@ -11,7 +11,21 @@
 	NoteController.prototype.displayNotes = function() {
 		appDiv = document.getElementById('app');
 		appDiv.innerHTML = this.noteListView.notesString();
-	}; 
+	};
+
+	window.addEventListener("hashchange", function(event) {
+		event.preventDefault();
+		if (window.location.hash === "") {
+			noteController.displayNotes();
+		} else {
+			new SingleNoteView(noteController.noteList.notes[getIdFromUrl()]).display();
+		}
+	});
+
+	function getIdFromUrl() {
+		return window.location.hash.split("#")[1].split("/")[1]
+	}
 
 	exports.NoteController = NoteController;
 })(this);
+	
