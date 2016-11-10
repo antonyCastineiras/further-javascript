c49dad49beafcdb38b637b2bb081559568f19195
Tomy8s <tomy8s@hotmail.com>
2016-11-10 12:00:54 +0000

    added test for note IDs

--- test.js
+++ test.js
@@ -21,7 +21,7 @@ function itDisplaysASingleNote() {
 	note = new Note("something");
 	singleNote = new SingleNoteView(note);
 	singleNote.display();
-	if(document.getElementById('app').textContent === note.text ) {
+	if(document.getElementById('app').textContent === note.getText() ) {
 		console.log("it displays a single note: passed")
 	} else {
 		throw new Error("it displays a single note: failed");
@@ -32,11 +32,29 @@ function itAbbreviatesText() {
 	note = new Note("some text that is longer than 20 characters");
 	noteController.noteList.addNote(note);
 	noteController.displayNotes();
-	list = document.querySelector('#noteList li:nth-child(2)');
-	if (list.textContent.length === 20) {
+	list = document.querySelector('#noteList li');
+	if (list.textContent.length === 23) {
 		console.log("it abbreviates text: passed")
 	} else {
 		throw new Error("it abbreviates text: failed")
+	}
+}
+
+function notesHaveIds() {
+	testNoteList = new NoteList();
+	note0 = new Note("This is note0");
+	testNoteList.addNote(note0);
+	note1 = new Note("This is note1");
+	testNoteList.addNote(note1);
+	if (note0.id() === 0) {
+		console.log("note0 has an id: passed");
+	} else {
+		throw new Error("note0 has an id: passed");
+	}
+	if (note1.id() === 1) {
+		console.log("note1 has an id: passed");
+	} else {
+		throw new Error("note1 has an id: passed");
 	}
 }
 
@@ -44,3 +62,4 @@ isTextCorrect();
 itDisplaysNotes();
 itDisplaysASingleNote();
 itAbbreviatesText();
+notesHaveIds();
